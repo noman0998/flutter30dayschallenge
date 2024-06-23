20d2dbfb4c825729f52cd55f62e664d6b9d71468
noman0998 <lagylagy754@gmail.com>
2024-06-23 23:15:24 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:test_project/Day6.dart';
 import 'package:test_project/day3.dart';
 import 'package:test_project/day4.dart';
 import 'package:test_project/day5.dart';
 import 'package:test_project/image_page.dart';
 import 'package:test_project/login_page.dart';
 import 'package:test_project/sign_up_page.dart';
-
 import 'tik_tok_ui_examle_stack.dart';
 
 void main() {
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Flutter 30 days challange",
-      home: TikTokUiExamleStack(),
+      home: MyStateFullWidget(),
     );
   }
 }
