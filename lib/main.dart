a7fbb2f8278b79bb5ed8532a2015a89d0c632f44
noman0998 <lagylagy754@gmail.com>
2024-06-24 23:31:23 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:test_project/Day6.dart';
 import 'package:test_project/day3.dart';
 import 'package:test_project/day4.dart';
 import 'package:test_project/day5.dart';
+import 'package:test_project/home_page.dart';
 import 'package:test_project/image_page.dart';
 import 'package:test_project/login_page.dart';
 import 'package:test_project/sign_up_page.dart';
@@ -17,9 +18,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: "/signUpPage",
+      routes: {
+        "/":(context) => Homepage(),
+        "/login":(context) => LoginPage(),
+        '/signUpPage':(context) => SignUpPage(),
+      },
       debugShowCheckedModeBanner: false,
       title: "Flutter 30 days challange",
-      home: MyStateFullWidget(),
+     // home: MyStateFullWidget(),
     );
   }
+
 }
